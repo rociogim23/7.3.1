fc6c0c818a951a35c79dee9b0e0500b679c5d485
Rodrigo Gorga <rodrigogorga7@gmail.com>
2023-08-09 18:41:57 -0300

    TA PRONTO

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -5,3 +5,42 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+var boton= document.getElementById("regBtn")
+boton.addEventListener("click",function(){
+var inputs= document.getElementsByTagName("input")
+
+
+    for (var i=0; i<inputs.length; i++){ 
+        if (inputs[i].value.length<=0){
+            showAlertError()
+            return null
+    }
+    }
+
+
+
+var contraseña= document.getElementById("password1")
+if (contraseña.value.length<5){
+    showAlertError()
+    return null
+}
+
+var contraseña2= document.getElementById("password2")
+if (contraseña.value !== contraseña2.value){
+    showAlertError()
+    return null
+}
+
+var checkbox = document.getElementById("terminos")
+console.log("checkbox", checkbox.checked)
+if (checkbox.checked === false){
+    showAlertError()
+    return null
+}
+
+showAlertSuccess()
+
+
+})
